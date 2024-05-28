8a8560d73dd7ca70b729e11128ce1da3ce3e8124
VoinHrista <68071855+VoinHrista@users.noreply.github.com>
2024-05-28 21:02:53 +0300

    Add files via upload

---
+++ src/main/kotlin/com/example/RedisClient.kt
@@ -0,0 +1,13 @@
+package com.exampleimport redis.clients.jedis.Jedis
+
+object RedisClient {
+    private val jedis = Jedis("93.95.97.113", 5432)
+
+    fun pushToQueue(queueName: String, message: String) {
+        jedis.rpush(queueName, message)
+    }
+
+    fun popFromQueue(queueName: String): String? {
+        return jedis.lpop(queueName)
+    }
+}
